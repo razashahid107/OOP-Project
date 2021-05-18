22cdc423131e235a450123b0311b3eed251ac33b
razashahid107 <razashahid107@gmail.com>
2021-05-19 01:26:35 +0500

    another try run. commiting all the files here

---
+++ notes2.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+// ntoes
+
+class Login
+{
+private:
+    string email, password;
+public:
+    // we can have a contructor for the login
+    Login(){
+        cout << "Please enter your login credentials: " << endl;
+        setEmail();
+        setPassword();
+    }
+    // setters and getters
+    void setEmail()
+    {
+        cout << endl
+             << "E-Mail: ";
+        cin >> email;
+    }
+    void setPassword()
+    {
+        cout << "Password: ";
+        cin >> password;
+    }
+    string getEmail()
+    {
+        return email;
+    }
+    string getPassword()
+    {
+        return password;
+    }
+};
+
+class Note : public Login
+{
+    // date and time for note creation
+private:
+    string note_data;
+    string note_title;
+
+public:
+    Note(){
+        char choice_input_char;
+        int choice_input_int;
+        cout << "Do you want to create a new note? If yes then press y otherwise press n" << endl << "\t=> ";
+        cin >> choice_input_char;
+        if (choice_input_char == 'y') { AddNote(); }
+        else if (choice_input_char == 'n') { 
+            cout << "What do you want to do?" << endl; 
+            cout << "\t1. Edit a previous note" << endl;
+            cout << "\t2. Delete a previous note" ;
+            cout << endl;
+            cin >> choice_input_int;
+            if (choice_input_int == 1) {
+                EditNote();
+            }
+            else if (choice_input_int == 2){
+                DelNote();
+            }
+        }
+    }
+    void AddNote() {
+        int int_choice;
+        // set note title
+        cout << "Do you want to have a title" << endl << "\t1. Yes" << endl << "\t0. No\t" << "=> ";
+        cin >> int_choice;
+        while (true) {    
+            if (int_choice == 1){
+                setNote_Title();
+            }
+            else if (int_choice == 0) {
+                break;
+            }
+        }
+        // since the while loop will break anyways, we'll now get to setting the data of the note
+        setData();
+    }
+    void setNote_Title() {   
+        cout << "Title: ";
+        cin >> note_title;        
+    }
+    string getNote_Title(){
+        return note_title;
+    }
+    void setData(){
+        cout << "Enter your Text: " << endl;
+        cin >> note_data;
+    }
+    string getData(){
+        return note_data;
+    }
+
+    void DelNote() {}
+    void EditNote() {}
+};
+
+int main()
+{
+    // you will first need to login
+    // I'll assume that the man is registerd
+    Note person1;
+    cout << endl;
+}
---
+++ test.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+#include <fstream>
+int main(){
+    fstream MyReadFile("data.txt");
+}
